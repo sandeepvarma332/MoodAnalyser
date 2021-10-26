d6faedad4ba555bcf335523467bba0d25fe9b5b1
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-26 13:43:55 +0530

    Added:welcome mesaage

---
+++ src/main/java/org/blz/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,10 @@
+package org.blz.moodanalyser;
+
+
+public class MoodAnalyser
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "HWelcome to mood analyser program" );
+    }
+}
