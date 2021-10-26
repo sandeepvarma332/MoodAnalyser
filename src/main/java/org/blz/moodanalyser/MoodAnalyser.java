0de84eff83e9d34f9f0572bb9ba4185bbabab353
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-26 13:46:14 +0530

    Added: Analysing mood

--- src/main/java/org/blz/moodanalyser/MoodAnalyser.java
+++ src/main/java/org/blz/moodanalyser/MoodAnalyser.java
@@ -3,8 +3,16 @@ package org.blz.moodanalyser;
 
 public class MoodAnalyser
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "HWelcome to mood analyser program" );
+    public static String analyseHappyMood(String message) {
+        if (message.contains("any") || message.contains("ANY")) {
+            return "happy";
+        } else
+            return "sad";
+    }
+    public static String analyseSadMood(String message) {
+        if (message.contains("sad") || message.contains("SAD")) {
+            return "sad";
+        } else
+            return "happy";
     }
 }
